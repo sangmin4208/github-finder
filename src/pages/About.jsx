af654a4372ee7444ad2f39b8a90cc9131ed94b11
이상민 <85425026+sangmin4208@users.noreply.github.com>
2023-05-25 15:42:05 +0900

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ const About = () => {
     <>
       <h1 className="text-6xl mb-4 text-white">Github Finder</h1>
       <p className="mb-4 text-2xl font-light text-white">
-        리액트로 만든 사이트 입니다. GitGub 프로필과 상세정보를 확인할 수
+        리액트로 만든 사이트 입니다. Github 프로필과 상세정보를 확인할 수
         있습니다.
       </p>
     </>
